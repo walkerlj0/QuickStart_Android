5b1d07dfd1609fe12fbc5894b9f1ce1e34e38546
Lindsay Walker <lindsay.waklker@saucelabs.com>
2020-12-02 11:40:09 -0600

    Changed 2.02 to 2.03

---
+++ Mod2/2.03/src/test/java/tests/BasicTest.java
@@ -0,0 +1,105 @@
+package tests;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+
+
+public class BasicTest {
+
+    private static final String APP = "/Users/lindsaywalker/Documents/Example_Tests/Android.SauceLabs.Mobile.Sample.app.2.7.0.apk";
+    private static final String APPIUM = "http://localhost:4723/wd/hub";
+
+    private String sessionId;
+    private AndroidDriver driver;
+
+    String usernameID = "test-Username";
+    String passwordID = "test-Password";
+    String submitButtonID = "test-LOGIN";
+    By ProductTitle = By.xpath("//android.widget.TextView[@text='PRODUCTS']");
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "Android Emulator"); //This will change to type of device e.g. Pixel 4 on Saucelabs
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion","9.0" ); //add platformVersion
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("appWaitActivity", "com.swaglabsmobileapp.MainActivity");
+        capabilities.setCapability("app", APP);
+
+
+        driver = new AndroidDriver(new URL(APPIUM), capabilities);
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+
+    }
+
+    @Test
+    public void loginToSwagLabsTestValid() {
+        System.out.println("Sauce - Start loginToSwagLabsTestValid test");
+
+        login("standard_user", "secret_sauce");
+
+        // Verification
+        Assert.assertTrue(isOnProductsPage());
+    }
+
+    @Test
+    public void loginTestValidProblem() {
+        System.out.println("Sauce - Start loginTestValidProblem test");
+
+        login("problem_user", "secret_sauce");
+
+        // Verification - we on Product page
+        Assert.assertTrue(isOnProductsPage());
+    }
+
+    public void login(String user, String pass){
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        final WebElement usernameEdit = wait.until(ExpectedConditions.visibilityOfElementLocated(new MobileBy.ByAccessibilityId(usernameID)));
+
+        usernameEdit.click();
+        usernameEdit.sendKeys(user);
+
+        WebElement passwordEdit = driver.findElementByAccessibilityId(passwordID);
+        passwordEdit.click();
+        passwordEdit.sendKeys(pass);
+
+        WebElement submitButton = driver.findElementByAccessibilityId(submitButtonID);
+        submitButton.click();
+    }
+
+    public boolean isOnProductsPage() {
+        //Create an instance of a Appium explicit wait so that we can dynamically wait for an element
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+
+        //wait for the product field to be visible and store that element into a variable
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(ProductTitle));
+        } catch (TimeoutException e){
+            System.out.println("*** Timed out waiting for product page to load.");
+            return false;
+        }
+        return true;
+    }
+}
+
